a9180bcf8254b03bbe4f727d3698d1d3ae8f1b0b
PROVIDEMEF9 <nancynandani241999@gmail.com>
2025-04-16 22:15:28 +0530

    modechange

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:import.meta.env.REACT_APP_MODE=== "development" ?"http://localhost:5001/api" :"/api",
+    baseURL: (import.meta?.env?.MODE || "production") === "development" ?"http://localhost:5001/api" :"/api",
     withCredentials:true,
 })
